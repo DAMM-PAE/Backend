0e4ed02e1ffaaa83bfeae6ebde6873a75eb95d65
Haopeng138 <haopeng138@gmail.com>
2023-10-30 00:01:48 +0100

    a

--- dammproject/dammproject/urls.py
+++ dammproject/dammproject/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path("", include("predictApp.urls")),
+    #path("", include("predictApp.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("predictBeers.urls")),
 
