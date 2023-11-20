5eaa5bdf72315d1155ca98a4668892f855df4728
Haopeng138 <haopeng@alstp.com>
2023-11-20 17:52:46 +0100

    user jwt

--- dammproject/dammproject/settings.py
+++ dammproject/dammproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import datetime
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,9 +25,11 @@ SECRET_KEY = "django-insecure-o!4hc^qjy2w)l-3$t^@76#$+$b&3epefu3#p^t(wvo(-@kh94r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://nattech.fib.upc.edu:40540","localhost", "0.0.0.0", "127.0.0.1","*","https://manually-pretty-barnacle.ngrok-free.app"]
+ALLOWED_HOSTS = ["http://nattech.fib.upc.edu:40540", "localhost", "0.0.0.0",
+                 "127.0.0.1", "*", "https://manually-pretty-barnacle.ngrok-free.app"]
 
-CSRF_TRUSTED_ORIGINS = ['https://manually-pretty-barnacle.ngrok-free.app','http://nattech.fib.upc.edu:40540']
+CSRF_TRUSTED_ORIGINS = [
+    'https://manually-pretty-barnacle.ngrok-free.app', 'http://nattech.fib.upc.edu:40540']
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "predictBeers",
     "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
+    "dj_rest_auth",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -51,7 +59,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware"
+    "django.middleware.common.CommonMiddleware",
+    "allauth.account.middleware.AccountMiddleware"
 ]
 
 ROOT_URLCONF = "dammproject.urls"
@@ -125,7 +134,7 @@ STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, '../static/'),
+    os.path.join(SITE_ROOT, '../static/'),
 )
 
 # Default primary key field type
@@ -133,12 +142,54 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+         'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
+}
 
-
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+#     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+# }
 # CORS CONFIG
 CORS_ORIGIN_WHITELIST = [
     "http://nattech.fib.upc.edu:40540",
     "http://localhost:3000",
-    
+
 ]
+
+# REST_AUTH_SERIALIZERS = {
+#     'USER_DETAILS_SERIALIZER': 'mobile.serializers.UserSerializer'
+# }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+AUTH_PROFILE_MODULE = 'users.MyUser'
+
+AUTH_USER_MODEL = 'users.MyUser'
+
